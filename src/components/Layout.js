95a0f74638481579d9ad55f614eec87a6d0f0c39
Yosuke Konno <yokohey@outlook.com>
2020-05-11 00:58:01 +0900

    update to bulma 0.8.2 and apply 2 column layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,7 @@ import Helmet from 'react-helmet'
 
 import Navbar from './Navbar'
 import Footer from './Footer'
+import Sidebar from './Sidebar'
 import './all.sass'
 import './scss/style.scss'
 
@@ -10,7 +11,10 @@ const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="Home | Gatsby + WordPress" />
     <Navbar />
-    <div>{children}</div>
+    <main className="columns">
+      <div className="column">{children}</div>
+      <Sidebar />
+    </main>
     <Footer />
   </div>
 )
---
+++ src/components/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar = () => (
+  <div className="submenu column is-3">
+    side menu
+  </div>
+)
+
+export default Sidebar
