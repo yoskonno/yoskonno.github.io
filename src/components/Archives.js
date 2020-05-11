973b708bc6287dc0a61b79ab85a532f6f45ad084
Yosuke Konno <yokohey@outlook.com>
2020-05-12 06:30:08 +0900

    Archive取得できた！

--- src/components/Archives.js
+++ src/components/Archives.js
@@ -1,15 +1,51 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import path from 'path'
+import { Link, StaticQuery, graphql } from 'gatsby'
+import _ from 'lodash'
 
-const Archives = () => {
-  const archives = path.resolve()
+const Archives = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allWordpressPost {
+          edges {
+            node {
+              id
+              slug
+              status
+              date
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      // TODO: duplicate code lines with gatsby-node.js
+      const getOnlyPublished = edges =>
+        _.filter(edges, ({ node }) => node.status === 'publish')
+      const listOfMonths = []
+      const allPosts = data.allWordpressPost.edges
+      const posts =
+        process.env.NODE_ENV === 'production'
+          ? getOnlyPublished(allPosts)
+          : allPosts
 
-  return (
-    <div>
-      <h4>Archives</h4>
-    </div>
-  )
-}
+      _.each(posts, ({ node: post }) => {
+        // Create the Gatsby page for this WordPress post
+        const date = new Date(post.date)
+
+        // create list of months
+        const yearAndMonth = `${String(date.getFullYear()).padStart(2, '0')}/${String(date.getMonth() + 1).padStart(2, '0')}`
+        if (!listOfMonths.includes(yearAndMonth)) {
+          listOfMonths.push(yearAndMonth)
+        }
+      })
+      return (
+        <div>
+          <h4>Archives</h4>
+          <p>{listOfMonths}</p>
+        </div>
+    )}}
+  />
+)
 
 export default Archives
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Archives from './Archives'
 
 const Sidebar = () => (
   <div className="submenu column is-3">
     side menu
+    <Archives />
   </div>
 )
 
