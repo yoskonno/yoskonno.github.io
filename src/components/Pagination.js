2ec1d22bf7432e6777a889b697439b9ba016e7ed
Yosuke Konno <yokohey@outlook.com>
2020-05-25 13:33:40 +0900

    complete basic pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -14,36 +14,67 @@ const Pagination = ({ pageContext, pathPrefix }) => {
 
   return (
     <nav className="pagination" role="navigation">
-      <div className="navbar navbar-menu">
-        {previousPagePath && (
-          <div className="navbar-item">
-            <Link to={previousPagePath} rel="prev">
-              Previous
-            </Link>
+      {previousPagePath && (
+        <div className="pagination__button-large">
+          <Link to={previousPagePath} rel="prev">
+            Previous
+          </Link>
+        </div>
+      )}
+      {threePathsBelow.reverse().map((path) => {
+        return (
+          <div className="pagination__button-number">
+            <a href={path.path}>{path.number}</a>
           </div>
-        )}
-        <p>humanPageNumber: {humanPageNumber}</p>
-        <p>numberOfPages: {numberOfPages}</p>
-        <p>threePathsBelow: {threePathsBelow}</p>
-        <p>threePathsAbove: {threePathsAbove}</p>
-        {nextPagePath && (
-          <div className="navbar-item">
-            <Link to={nextPagePath} rel="next">
-              Next
-            </Link>
-          </div>
-        )}
+        )
+      })}
+      <div className="pagination__button-number pagination__button-number--current">
+        {humanPageNumber}
       </div>
+      {threePathsAbove.map((path) => {
+        return (
+          <div className="pagination__button-number">
+            <a href={path.path}>{path.number}</a>
+          </div>
+        )
+      })}
+      {nextPagePath && (
+        <div className="pagination__button-large">
+          <Link to={nextPagePath} rel="next">
+            Next
+          </Link>
+        </div>
+      )}
     </nav>
   )
 }
 
 const getThreePathsBelow = (humanPageNumber, numberOfPages) => {
-  
+  const threePathsBelow = []
+  console.log(`threePathsBelow: humanPageNumber: ${humanPageNumber}`)
+  for (let i = humanPageNumber - 1; i >= humanPageNumber - 3 ; i-- ) {
+    if (i > 0) {
+      threePathsBelow.push({
+        number: i,
+        path: `/page/${i}`,
+      })
+    }
+  }
+  return threePathsBelow
 }
 
-const getThreePathsAbove = () => {
-  
+const getThreePathsAbove = (humanPageNumber, numberOfPages) => {
+  const threePathsAbove = []
+  console.log(`threePathsAbove: humanPageNumber: ${humanPageNumber}`)
+  for (let i = humanPageNumber + 1; i <= humanPageNumber + 3 ; i++ ) {
+    if (i <= numberOfPages) {
+      threePathsAbove.push({
+        number: i,
+        path: `/page/${i}`,
+      })
+    }
+  }
+  return threePathsAbove
 }
 
 export default Pagination
