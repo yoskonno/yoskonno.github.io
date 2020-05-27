35062477712f5e9aca832a27fc4be256cb4b44ee
Yosuke Konno <yokohey@outlook.com>
2020-05-27 15:28:13 +0900

    avoid using es6 module in gatsby-node and helper

--- src/lib/helper/timeHelper.js
+++ src/lib/helper/timeHelper.js
@@ -1,16 +1,20 @@
-import moment from 'moment-timezone'
+const moment = require('moment-timezone')
 
-export const getPathFromDate = (date) => {
+const getPathFromDate = (date) => {
   const jstMoment = moment.tz(date, 'Asia/Tokyo')
 
   const pathFromDate = `${jstMoment.year()}/${String(jstMoment.month() + 1).padStart(2, '0')}/${String(jstMoment.date()).padStart(2, '0')}`
   return pathFromDate
 }
 
-export const getYearAndMonthString = (date) => {
+const getYearAndMonthString = (date) => {
   const jstMoment = moment.tz(date, 'Asia/Tokyo')
 
   const yearAndMonthString = `${jstMoment.year()}/${String(jstMoment.month() + 1).padStart(2, '0')}`
   return yearAndMonthString
 }
 
+module.exports = {
+  getPathFromDate,
+  getYearAndMonthString
+}
